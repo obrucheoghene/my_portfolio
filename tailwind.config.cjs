09e0e422156ce9c070a6c4fa60f3e06c2a28856f
wilfredcloud <wilfredcloudspace@gmail.com>
2023-03-01 20:26:57 +0100

    Add custom color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: '#201F1F',
+    },
     extend: {
       container: {
         center: true,
