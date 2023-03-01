a2034fde724befc432257f6a19be656ad13627d2
wilfredcloud <wilfredcloudspace@gmail.com>
2023-03-01 20:27:44 +0100

    Test custom color

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import { useState } from 'react';
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="dark">
+      <div className=" bg-white text-black dark:bg-black h-56 dark:text-white">
+        Gloru
+      </div>
+    </div>
+  );
 }
 
 export default App;
