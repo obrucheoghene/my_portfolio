17070651b79e457e74c5c068ab5a175cff134554
wilfredcloud <wilfredcloudspace@gmail.com>
2023-05-09 18:52:39 +0100

    fix: Update ui

--- src/App.tsx
+++ src/App.tsx
@@ -2,31 +2,36 @@ import { useState } from 'react';
 
 const App = () => {
   return (
-    <div className="">
-      <div className=" bg-main-bg text-white">
-        {/* Navbar */}
-        <div className=" container h-20 flex flex-row justify-between items-center ">
-          <div>OW</div>
-          <div className=" flex flex-row gap-x-6">
-            {['Projects', 'Resume', 'Contact'].map((item) => (
-              <span>{item}</span>
-            ))}
+    <div className="bg-main-bg">
+      <div className="  container">
+        <div className=" text-white h-screen flex flex-col justify-between">
+          {/* Navbar */}
+          <div className="h-20 flex flex-row justify-between items-center">
+            <div>OW</div>
+            <div className=" flex flex-row gap-x-6">
+              {['Projects', 'Resume', 'Contact'].map((item) => (
+                <span>{item}</span>
+              ))}
+            </div>
           </div>
-        </div>
-        {/* Hero */}
-        <div className=" container py-12">
-          <p>Hey I'm</p>
-          <h1 className=" text-3xl my-10">Obruche Wilfred</h1>
-          <p className=" my-4">
-            An experienced web developer with more than four years of practice
-            in developing dynamic, scalable, and high-performance web
-            applications. Possesses strong skills in Javascript, PHP, Node.js,
-            React.js, Git, SQL (MySQL), NoSQL (Mongodb), CSS, Tailwind, and
-            Bootstrap CSS. A performer, capable of delivering high-quality and
-            reusable code. Excited to tackle more intricate problems and
-            continuously explore innovative ways to enhance users experience.
-          </p>
-          <p>Say hi --- </p>
+          {/* Hero */}
+          <div className="flex flex-col gap-6">
+            <p>Hey I'm</p>
+            <h1 className="text-3xl  primary-gd ">Obruche Wilfred</h1>
+            <p className="text-justify leading-8">
+              An experienced web developer with more than four years of practice
+              in developing dynamic, scalable, and high-performance web
+              applications. Possesses strong skills in Javascript, PHP, Node.js,
+              React.js, Git, SQL (MySQL), NoSQL (Mongodb), CSS, Tailwind, and
+              Bootstrap CSS. A performer, capable of delivering high-quality and
+              reusable code. Excited to tackle more intricate problems and
+              continuously explore innovative ways to enhance users experience.
+            </p>
+          </div>
+
+          <div>
+            <h1>FEATURE PROJECTS</h1>
+          </div>
         </div>
       </div>
     </div>
